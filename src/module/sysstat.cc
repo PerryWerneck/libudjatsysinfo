902f1d2a9093f5cd04280d7168fd18f3ded0b851
Perry Werneck <perry.werneck@gmail.com>
2021-08-07 10:52:04 -0300

    Using System::Stats class from libudjat.

--- src/module/sysstat.cc
+++ src/module/sysstat.cc
@@ -38,53 +38,6 @@
 		PACKAGE_BUGREPORT 											// The bug report address.
 	};
 
-	static const struct {
-		const char *name;
-		const char *label;
-		const char *summary;
-	} stats[] = {
-		{
-			"users",
-			"Normal processes",
-			"CPU used by normal processes executing in user mode"
-		},
-		{
-			"niced",
-			"Niced processes",
-			"CPU used by niced processes executing in user mode"
-		},
-		{
-			"kernel",
-			"Kernel mode",
-			"CPU used by processes executing in kernel mode"
-		},
-		{
-			"idle",
-			"IDLE",
-			"twiddling thumbs"
-		},
-		{
-			"iowait",
-			"Waiting for I/O",
-			"CPU waiting for I/O to complete"
-		},
-		{
-			"irq",
-			"Interrupts",
-			"CPU used when servicing interrupts"
-		},
-		{
-			"sirq",
-			"Soft IRQS",
-			"CPU used when servicing softirqs"
-		},
-		{
-			"total",
-			"Total CPU used by all processes and interrupts",
-			"Total use of CPU"
-		}
-	};
-
 	static System::Stat::Type getFieldFromNode(const xml_node &node) {
 		return System::Stat::getIndex(Attribute(node,"field-name").as_string("total"));
 	}
@@ -153,12 +106,11 @@
 
 
 	public:
-		Agent(const xml_node &node) : Abstract::Agent("sysstat") {
+		Agent(const xml_node &node) : Abstract::Agent("sysstat"), type(getFieldFromNode(node)) {
 
 			this->icon = "utilities-system-monitor";
-			this->type = getFieldFromNode(node);
-			this->summary = stats[this->type].summary;
-			this->label = stats[this->type].label;
+			this->summary = System::Stat::getSummary(this->type);
+			this->label = System::Stat::getLabel(this->type);
 
 			Abstract::Agent::load(node);
 
@@ -182,7 +134,7 @@
 
 			auto &cpu = response["details"];
 			for(size_t ix = 0; ix < N_ELEMENTS(values); ix++) {
-				cpu[stats[ix].name].setFraction(values[ix]);
+				cpu[System::Stat::typenames[ix]].setFraction(values[ix]);
 			}
 
 		}
@@ -191,10 +143,10 @@
 
 			report.start("name","label","summary","value",nullptr);
 
-			for(size_t ix = 0; ix < N_ELEMENTS(stats); ix++) {
-				report 	<< stats[ix].name
-						<< stats[ix].label
-						<< stats[ix].summary
+			for(size_t ix = 0; ix < N_ELEMENTS(values); ix++) {
+				report 	<< System::Stat::typenames[ix]
+						<< System::Stat::getLabel((System::Stat::Type) ix)
+						<< System::Stat::getSummary((System::Stat::Type) ix)
 						<< (values[ix] * 100);
 
 			}
@@ -211,46 +163,16 @@
 			System::Stat current;
 			System::Stat diff(current);
 
-			diff -= this->saved;	// Get only the diference.
+			diff -= this->saved;	// Get only the difference.
 			this->saved = current;	// Update saved value.
-
-			cout << "USER=" << diff.user << " IDLE=" << diff.idle << " TOTAL=" << diff.total() << endl;
 
 			float total = (float) diff.total();
 			for(size_t ix = 0; ix < N_ELEMENTS(values); ix++) {
 				values[ix] = ((float) diff[(System::Stat::Type) ix]) / total;
 #ifdef DEBUG
-				cout << stats[ix].name << " = " << fixed << setprecision(2) << (values[ix] * 100) << " %" << endl;
+				cout << System::Stat::typenames[ix] << " = " << fixed << setprecision(2) << (values[ix] * 100) << " %" << endl;
 #endif // DEBUG
 			}
-
-			/*
-			// Get current system usage.
-			unsigned long current[N_ELEMENTS(stats)];
-			read(current);
-
-			// Compute differences from last cycle.
-			float total = 0;
-			for(size_t ix = 0; ix < N_ELEMENTS(stats); ix++) {
-				unsigned long up = current[ix];
-				if(current[ix] >= saved[ix]) {
-					current[ix] -= saved[ix];
-				} else {
-					current[ix] = 0;
-				}
-				total += (float) current[ix];
-				saved[ix] = up;
-			}
-
-			// Compute values on %
-			for(size_t ix = 0; ix < N_ELEMENTS(stats); ix++) {
-
-				values[ix] = ((float) current[ix]) / total;
-
-
-			}
-
-			*/
 
 			updated(true);
 			return true;
