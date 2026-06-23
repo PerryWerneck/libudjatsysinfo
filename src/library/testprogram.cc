0b8de60bc9d53c04ab5ef525f0dedbc298885bc5
Perry Werneck <perry.werneck@gmail.com>
2026-06-22 22:13:23 -0300

    Updating to latest libudjat.

--- src/library/testprogram.cc
+++ src/library/testprogram.cc
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: LGPL-3.0-or-later */
 
 /*
- * Copyright (C) 2021 Perry Werneck <perry.werneck@gmail.com>
+ * Copyright (C) 2025 Perry Werneck <perry.werneck@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
@@ -19,30 +19,41 @@
 
  #include <config.h>
  #include <udjat/defs.h>
- #include <udjat/tools/loader.h>
- #include <udjat/module.h>
+ #include <udjat/tools/logger.h>
+ #include <udjat/tools/unit-test.h>
+ #include <string>
  #include <udjat/agent/percentage.h>
  #include <udjat/tools/storage/stat.h>
 
- #include <iostream>
-
  using namespace Udjat;
  using namespace std;
- 
- int main(const int argc, const char **argv) {
-	return loader(argc,argv,[](Application &app) -> int {
 
-		{
+ #ifdef DEBUG 
 
+ UDJAT_API void enum_udjat_unit_tests(Udjat::UnitTests &tests) noexcept {
+
+	tests.append(
+		UnitTests::Worker{
+			"Test percent agent",
+			[]() {				
+				Agent<Percentage> percent{"test-percent",0.1};
+				debug("Expanding----> '",String{"The percent value is ${value}"}.expand(percent).c_str(),"'");
+				return true;
+			}
+		},
+		UnitTests::Worker{
+			"Test storage enumerator",
+			[]() {
+				for(const auto &disk : Storage::Stat::get()) {
+					debug("----> Disk '",disk.name(),"' is ",(disk.physical() ? "Physical" : "Not physical"));
+				}
+				return true;
+			}
 		}
+	);
 
-		for(const auto &disk : Storage::Stat::get()) {
-			debug("----> Disk '",disk.name(),"' is ",(disk.physical() ? "Physical" : "Not physical"));
-		}
+ }  
 
-		return 0;
+ #endif
 
-	});
-
- }
 
